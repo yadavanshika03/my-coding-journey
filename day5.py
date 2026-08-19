939b8ddb5c067398e27f3f7adad58934df43a8f6
yadavanshika03 <yadavanshika5171@gmail.com>
2026-08-19 08:48:07 +0530

    Create day5.py

---
+++ day5.py
@@ -0,0 +1,9 @@
+# hey anshika, please dont remove this line
+print("hey i am a good girl\nand this viewer is also a good boy/girl ")
+#ctrl+/
+#print("hello anshika")
+#print("i am coming lbsnaa")
+print("hey\' i am a \"good girl\"\nand this viewer is also is a good boy/girl")
+print("hey,6,7")
+print("hey",6,7,sep="~",end="9\n")
+print("anshika")
