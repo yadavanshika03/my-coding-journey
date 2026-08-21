d3f505cc88d385351e8e167aa543c6ec452263be
yadavanshika03 <yadavanshika5171@gmail.com>
2026-08-21 05:59:23 +0530

    Create day11.py

---
+++ day11.py
@@ -0,0 +1,20 @@
+name="Anshika"
+friend="Angel"
+anotherfriend="kanishka"
+apple='''he said,
+hi Anshika
+hey I am good
+"i want to eat an apple'''
+
+print("hello,"+name)
+print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print("lets use a for loop\n")
+for character in apple:
+	print(character)
