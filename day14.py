9022dc6b201ccfe8f24711e4fa2f1789a2db223c
yadavanshika03 <yadavanshika5171@gmail.com>
2026-08-23 06:16:08 +0530

    Create day14.py

---
+++ day14.py
@@ -0,0 +1,71 @@
+a=int(input("enter your age:"))
+print("your age is a:",a)
+#conditional operators
+#>,<,>=,<=,==,!=
+#print(a>18)
+#print(a<=18)
+#print(a==18)
+#print(a!=18)
+if(a>18):
+	print("you can drive")
+	print("yes")
+else:
+     print("you cannot drive")
+     print("no")
+print("Yay!")
+
+applePrice = 10
+budget = 200
+if (budget - applePrice > 50):
+    print("Alexa, add 1 kg Apples to the cart.")
+else:
+    print("Alexa, do not add Apples to the cart.")
+ 
+applePrice = 210
+budget = 200
+if (budget - applePrice > 50):
+    print("Alexa, add 1 kg Apples to the cart.")
+else:
+    print("Alexa, do not add Apples to the cart.")
+    
+num = int(input("Enter the value of num: "))
+if (num < 0):
+    print("Number is negative.")
+elif (num == 0):
+    print("Number is Zero.")
+else:
+    print("Number is positive.")
+    
+num = int(input("Enter the value of num: "))
+if (num < 0):
+    print("Number is negative.")
+elif (num == 0):
+    print("Number is Zero.")
+elif (num == 999):
+    print("Number is Special.")
+else:
+    print("Number is positive.")
+    
+num = 18
+if (num < 0):
+    print("Number is negative.")
+elif (num > 0):
+    if (num <= 10):
+        print("Number is between 1-10")
+    elif (num > 10 and num <= 20):
+        print("Number is between 11-20")
+    else:
+        print("Number is greater than 20")
+else:
+    print("Number is zero")
+    
+import time
+timestamp = time.strftime('%H:%M:%S')
+print(timestamp)
+timestamp = time.strftime('%H')
+print(timestamp)
+timestamp = time.strftime('%M')
+print(timestamp)
+timestamp = time.strftime('%S')
+print(timestamp)
+#https://docs.python.org/3/library/time.html#time.strftime
