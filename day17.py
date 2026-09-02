bdf38ef2fc335652cec25fe583ab55ab84577c09
yadavanshika03 <yadavanshika5171@gmail.com>
2026-09-02 05:57:06 +0530

    Create day17.py

---
+++ day17.py
@@ -0,0 +1,21 @@
+name='Anshika'
+for i in name:
+	print(i)
+	if(i=="n"):
+		print("This is something special!")
+		
+colors = ["Red", "Green", "Blue", "Yellow"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+        
+for k in range(5):
+		print(k+1)
+		
+for k in range(1,10):
+		print(k)
+		
+for k in range(1,12,2):
+		print(k)
+		
