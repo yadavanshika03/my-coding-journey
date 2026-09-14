ae108dae2f2eb4284c688c56c0096ed9ecc9c220
yadavanshika03 <yadavanshika5171@gmail.com>
2026-09-14 06:56:14 +0530

    Update day14.py

--- day14.py
+++ day14.py
@@ -59,13 +59,3 @@ elif (num > 0):
 else:
     print("Number is zero")
     
-import time
-timestamp = time.strftime('%H:%M:%S')
-print(timestamp)
-timestamp = time.strftime('%H')
-print(timestamp)
-timestamp = time.strftime('%M')
-print(timestamp)
-timestamp = time.strftime('%S')
-print(timestamp)
-#https://docs.python.org/3/library/time.html#time.strftime
