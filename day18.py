96597bbcaaddc20cc69243da85e3b2b3b1fd5d7f
yadavanshika03 <yadavanshika5171@gmail.com>
2026-09-15 07:43:54 +0530

    Create day18.py

---
+++ day18.py
@@ -0,0 +1,21 @@
+i=int(input("enter the number:"))
+while(i<=38):
+	i=int(input("enter the number"))
+	print(i)
+	
+print("done with the loop")
+
+count=5
+while(count>0):
+	print(count)
+	count=count-1
+else:
+	print("i am inside else")
+	
+	
+#do{
+#loop body;
+#}while(condition);
+
+
+
