211439840c89156a637b3216931f1d04433a81f0
yadavanshika03 <yadavanshika5171@gmail.com>
2026-09-23 18:18:11 +0530

    Create day26.py

---
+++ day26.py
@@ -0,0 +1,19 @@
+countries = ("Spain", "Italy", "India", "England", "Germany")
+temp = list(countries)
+temp.append("Russia") #add item
+temp.pop(3) #remove item
+temp[2] = "Finland" #change item
+countries = tuple(temp)
+print(countries)
+
+countries = ("Pakistan", "Afghanistan", "Bangladesh", "ShriLanka")
+countries2 = ("Vietnam", "India", "China")
+southEastAsia = countries + countries2
+print(southEastAsia)
+
+tuple1 = (0, 1, 2, 4, 2, 31, 1, 3, 2, 3)
+res = tuple1.count(3)
+#res = tuple1.index(3)
+#res = tuple1.index(3, 4, 8)
+#res = len(tuple1)
+print('Count of 3 in tuple1 is:', res)
